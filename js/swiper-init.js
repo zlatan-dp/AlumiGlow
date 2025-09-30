d7710b5e4c366a34347909808570f72909e33473
zlatandp <zlatandnepr@gmail.com>
2025-09-30 17:50:15 +0300

    added tv-mount section mobile ver

---
+++ js/swiper-init.js
@@ -0,0 +1,14 @@
+const alumiglowSwiper = new Swiper('.alumiglow-swiper', {
+    slidesPerView: 1.2,
+    spaceBetween: 24,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        561: {
+            spaceBetween: 48,
+        },
+    },
+});
