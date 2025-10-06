e148af430e2b341ecdc0dcd7d54d49212da4c8ce
zlatandp <zlatandnepr@gmail.com>
2025-10-06 18:32:50 +0300

    added sundet section

--- js/swiper-init.js
+++ js/swiper-init.js
@@ -12,3 +12,18 @@ const alumiglowSwiper = new Swiper('.use-cases-swiper', {
         },
     },
 });
+
+const sunriseSunsetSwiper = new Swiper('.sunrise-sunset-swiper', {
+    slidesPerView: 1.2,
+    spaceBetween: 24,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        561: {
+            spaceBetween: 48,
+        },
+    },
+});
