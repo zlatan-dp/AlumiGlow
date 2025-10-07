f6bfe9a7e71e033f0dba12b908dd0d17a3908fdf
zlatandp <zlatandnepr@gmail.com>
2025-10-07 15:32:26 +0300

    added eyes_train & magnetic sections

--- js/swiper-init.js
+++ js/swiper-init.js
@@ -27,3 +27,18 @@ const sunriseSunsetSwiper = new Swiper('.sunrise-sunset-swiper', {
         },
     },
 });
+
+const magneticSwiper = new Swiper('.magnetic-swiper', {
+    slidesPerView: 1.2,
+    spaceBetween: 24,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        561: {
+            spaceBetween: 48,
+        },
+    },
+});
